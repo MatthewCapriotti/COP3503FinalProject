1bba19404d32872484f47c1dfe9732714994c55c
William Lee <will.joe.lee@gmail.com>
2018-04-09 12:11:12 -0400

    Updating file from last meeting

--- loadAndClose.cpp
+++ loadAndClose.cpp
@@ -10,7 +10,7 @@ void loadUser(string username)
     ifstream inFS ("save.txt");             // Input stream
     string line = "";                       // Current line in the text document
     bool foundUser = false;                 // Becomes true if the username is found in the text document
-    bool endOfUser = false;                       // Becomes false when at the end of a user's information
+    bool endOfUser = false;                 // Becomes false when at the end of a user's information
 
     // These declarations should not be necessary in the final program
     int weight;
@@ -18,9 +18,11 @@ void loadUser(string username)
     int age;
     string goal;                            // Set as a string for organization at this early stage
     vector<string> calorieIntake;
+    vector<string> exercisesVector;         // Set as a string vector for organization at this early stage
 
     string userHistory;                     // Set as a string for organization at this early stage
 
+    // TO BE USED WHEN "Exercise" BECOMES AVAILABLE
     string exerciseName;
     string muscleTargeted;
     bool isCardio;
@@ -38,34 +40,62 @@ void loadUser(string username)
                 foundUser = true;
             }
         }
-        cout << inFS.good() << endl;
     }
 
     // Iterates through user's information if found
     if(foundUser)
     {
-        while(inFS >> line && line != "endOfUser")
+        while(line != "endOfUser")
         {
-            if(line.substr(0,7) == "weight:")
-                weight = stoi(line.substr(7));
-            if(line.substr(0,7) == "height:")
-                height = stoi(line.substr(7));
-            if(line.substr(0,5) == "goal")
-                goal = line.substr(5);
-
-            if(line == "beginIntake")
+            // Checks for the user's saved weight
+            if (line.substr(0, 7) == "weight:")
             {
-                while(line != "endIntake")
+                weight = stoi(line.substr(7));
+            }
+            // Checks for the user's saved height
+            if (line.substr(0, 7) == "height:")
+            {
+                height = stoi(line.substr(7));
+            }
+            // Checks for the user's saved goal
+            if (line.substr(0, 5) == "goal:")
+            {
+                goal = line.substr(5);
+            }
+
+            // Begins checking intake values, line by line
+            if(line.substr(0,11) == "beginIntake")
+            {
+                inFS >> line;
+                while(line.substr(0,9) != "endIntake")
                 {
                     calorieIntake.push_back(line);
                     inFS >> line;
                 }
             }
 
+            // Begins checking exercises, line by line. ALL exercise info will be placed into each
+            // item in the vector, and will be parsed when the Exercise class becomes available.
+            if(line.substr(0,14) == "beginExercises")
+            {
+                inFS >> line;
+                while(line.substr(0,12) != "endExercises")
+                {
+                    cout << line << endl;
+                    exercisesVector.push_back(line);
+                    inFS >> line;
+                }
+            }
+
+            // Moves stream forward
             inFS >> line;
 
-            if(line == "endOfUser")
+            // If the line equals this statement, the loop will terminate as the user's
+            // information will have fully been checked.
+            if(line.substr(0,9) == "endOfUser")
+            {
                 break;
+            }
         }
     }
 

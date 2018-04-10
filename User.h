643c834def5c34f6ba736581c377ed39a07830c7
dgarzonmoreno <dgarzonmoreno@ufl.edu>
2018-04-10 15:26:46 -0400

    Updated User.h file. Delta's: userID -> username, set methods with proper parameters, added setUsername

--- User.h
+++ User.h
@@ -6,16 +6,16 @@
 class Actor
 {
     protected:
-        std::string userID;
+        std::string username;
         std::string password;
         bool isSignedIn;
 
     public:
         virtual bool userNameTaken(std::string possibleID);
-        virtual std::string getUserID(std::string referenceOfAuthentication) = 0;
+        virtual std::string getUsername(std::string referenceOfAuthentication) = 0;
         virtual std::string getPassword() = 0;
         virtual void setPassword(std::string newPassword) = 0;
-        virtual bool isAuthenticated(std::string userID, std::string password) = 0;
+        virtual bool isAuthenticated(std::string username, std::string password) = 0;
 };
 
 class member: public Actor
@@ -31,35 +31,36 @@ class member: public Actor
         std::string zipcode;
 
     public:
-        member(std::string userID, std::string password, std::string name, 
+        member(std::string username, std::string password, std::string name, 
                 int age, char gender, std::string email, 
                 std::string city, std::string state, std::string zipcode);
         member();
 
         //interface abstract methods
         bool userNameTaken(std::string possibleID);
-        bool isAuthenticated(std::string userID, std::string password);
-        std::string getUserID(std::string email);
+        bool isAuthenticated(std::string username, std::string password);
+        std::string getUsername(std::string email);
         std::string getPassword(std::string email);
         void setPassword(std::string newPassword);
 
         //independent functions for a member of Gainz Ville
-        std::string getName(std::string userID);
-        int getAge(std::string userID);
-        char getGender(std::string userID);
-        std::string getEmail(std::string userID);
-        std::string getPhoneNum(std::string userID);
-        std::string getCity(std::string userID);
-        std::string getState(std::string userID);
-        std::string getZipCode(std::string userID);
-        void setName(std::string userID);
-        void setAge(std::string userID);
-        void setGender(std::string userID);
-        void changeEmail(bool isAuthenticated(std::string userID, std::string password));
-        void changePhoneNum(bool isAuthenticated(std::string userID, std::string password));
-        void changeCity(bool isAuthenticated(std::string userID, std::string password));
-        void changeState(bool isAuthenticated(std::string userID, std::string password));
-        void changeZipCode(bool isAuthenticated(std::string userID, std::string password));
+        std::string getName(std::string username);
+        int getAge(std::string username);
+        char getGender(std::string username);
+        std::string getEmail(std::string username);
+        std::string getPhoneNum(std::string username);
+        std::string getCity(std::string username);
+        std::string getState(std::string username);
+        std::string getZipCode(std::string username);
+        void setName(std::string username, std::string name);
+        void setAge(std::string username, int age);
+        void setGender(std::string username, char gender);
+        void setUsername(std::string username, bool userNameTaken(std::string possibleID));
+        void changeEmail(bool isAuthenticated(std::string username, std::string password));
+        void changePhoneNum(bool isAuthenticated(std::string username, std::string password));
+        void changeCity(bool isAuthenticated(std::string username, std::string password));
+        void changeState(bool isAuthenticated(std::string username, std::string password));
+        void changeZipCode(bool isAuthenticated(std::string username, std::string password));
 };
 
 class admin: public Actor
@@ -67,11 +68,11 @@ class admin: public Actor
     private:
         std::string adminCode;
     public:
-        admin(std::string userID, std::string password, std::string adminCode);
+        admin(std::string username, std::string password, std::string adminCode);
         admin();
         bool userNameTaken(std::string possibleID);
-        bool isAuthenticated(std::string userID, std::string password);
-        std::string getUserID(std::string adminCode);
+        bool isAuthenticated(std::string username, std::string password);
+        std::string getUsername(std::string adminCode);
         std::string getPassword(std::string adminCode);
 };
 

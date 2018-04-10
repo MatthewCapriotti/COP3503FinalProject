4687fd383681679ad60343c5f54d68343c251757
dgarzonmoreno <dgarzonmoreno@ufl.edu>
2018-04-10 15:12:35 -0400

    Fixed User.h to match Will's I/O Module requirements

--- User.h
+++ User.h
@@ -6,14 +6,14 @@
 class Actor
 {
     protected:
-        std::string const userID;
+        std::string userID;
         std::string password;
         bool isSignedIn;
 
     public:
         virtual bool userNameTaken(std::string possibleID);
-        virtual const std::string getUserID(std::string referenceOfAuthentication) = 0;
-        virtual const std::string getPassword() = 0;
+        virtual std::string getUserID(std::string referenceOfAuthentication) = 0;
+        virtual std::string getPassword() = 0;
         virtual void setPassword(std::string newPassword) = 0;
         virtual bool isAuthenticated(std::string userID, std::string password) = 0;
 };
@@ -21,9 +21,9 @@ class Actor
 class member: public Actor
 {
     private:
-        std::string const name;
-        int const age;
-        char const gender;
+        std::string name;
+        int age;
+        char gender;
         std::string email;
         std::string phoneNum;
         std::string city;
@@ -33,24 +33,28 @@ class member: public Actor
     public:
         member(std::string userID, std::string password, std::string name, 
                 int age, char gender, std::string email, 
-                std::string city, std::string state, std::string zipcode):name(name),age(age),gender(gender)
+                std::string city, std::string state, std::string zipcode);
+        member();
 
         //interface abstract methods
         bool userNameTaken(std::string possibleID);
         bool isAuthenticated(std::string userID, std::string password);
-        const std::string getUserID(std::string email);
-        const std::string getPassword(std::string email);
+        std::string getUserID(std::string email);
+        std::string getPassword(std::string email);
         void setPassword(std::string newPassword);
 
         //independent functions for a member of Gainz Ville
-        const std::string getName(std::string userID);
-        const int getAge(std::string userID);
-        const char getGender(std::string userID);
+        std::string getName(std::string userID);
+        int getAge(std::string userID);
+        char getGender(std::string userID);
         std::string getEmail(std::string userID);
         std::string getPhoneNum(std::string userID);
         std::string getCity(std::string userID);
         std::string getState(std::string userID);
         std::string getZipCode(std::string userID);
+        void setName(std::string userID);
+        void setAge(std::string userID);
+        void setGender(std::string userID);
         void changeEmail(bool isAuthenticated(std::string userID, std::string password));
         void changePhoneNum(bool isAuthenticated(std::string userID, std::string password));
         void changeCity(bool isAuthenticated(std::string userID, std::string password));
@@ -64,10 +68,11 @@ class admin: public Actor
         std::string adminCode;
     public:
         admin(std::string userID, std::string password, std::string adminCode);
+        admin();
         bool userNameTaken(std::string possibleID);
         bool isAuthenticated(std::string userID, std::string password);
-        const std::string getUserID(std::string adminCode);
-        const std::string getPassword(std::string adminCode);
+        std::string getUserID(std::string adminCode);
+        std::string getPassword(std::string adminCode);
 };
 
 

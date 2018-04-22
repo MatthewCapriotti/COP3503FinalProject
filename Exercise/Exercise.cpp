e37777603937d5c735eacd49434723b3b5d80368
SantiagoMejia183 <32145854+SantiagoMejia183@users.noreply.github.com>
2018-04-22 17:40:24 -0400

    Update Exercise.cpp

--- Exercise/Exercise.cpp
+++ Exercise/Exercise.cpp
@@ -9,16 +9,34 @@
 Exercise::Exercise() = default; //Default constructor for exercises
 
 Exercise::Exercise(string exerciseName, string muscleTargeted, bool isCardio)
+    //Constructor arguments for exercises,
 {
     this->exerciseName = exerciseName;
     this->muscleTargeted = muscleTargeted;
     this->isCardio = isCardio;
 }
+//Constructor arguments for exercises
 
-void Exercise::setName(string exerciseName) {this->exerciseName = exerciseName;}
-void Exercise::setMuscle(string muscleTargeted) {this->muscleTargeted = muscleTargeted;}
-void Exercise::setCardio(bool isCardio) {this->isCardio = isCardio;}
+void Exercise::setName(string exerciseName) {
+    this->exerciseName = exerciseName;
+}
 
-string Exercise::getName(){return exerciseName;}
-string Exercise::getMuscle(){return muscleTargeted;}
-bool Exercise::getCardio(){return isCardio;}
+void Exercise::setMuscle(string muscleTargeted) {
+    this->muscleTargeted = muscleTargeted;
+}
+
+void Exercise::setCardio(bool isCardio) {
+    this->isCardio = isCardio;
+}
+
+string Exercise::getName(){
+    return exerciseName;
+}
+
+string Exercise::getMuscle(){
+    return muscleTargeted;
+}
+
+bool Exercise::getCardio(){
+    return isCardio;
+}
